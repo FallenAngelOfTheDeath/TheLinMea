66911238ec2e3b18136b1ae06a9e6780d331fd77
FallenAngelOfTheDeath <fallenangelofthedeath@gmail.com>
2017-08-30 23:19:17 +0300

    ...

--- app/src/main/java/com/fallenangel/linmea/authentication/LoginActivity.java
+++ app/src/main/java/com/fallenangel/linmea/authentication/LoginActivity.java
@@ -18,19 +18,16 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
-    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9#_~!$&'()*+,;=:.\"(),:;<>@\\[\\]\\\\]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*$";
-    private Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-    private Matcher matcher;
+//    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9#_~!$&'()*+,;=:.\"(),:;<>@\\[\\]\\\\]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*$";
+//    private Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+//    private Matcher matcher;
 
-    private String LOG_TAG = "LoginActivity";
+    //private String LOG_TAG = "Authentication";
 
     Button loginButton;
     EditText usernameEditText, passwordEditText;
@@ -53,12 +50,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
                     // User is signed in
-                    Log.d(LOG_TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Log.d(String.valueOf(getText(R.string.LOG_TAG_AUTH)), "onAuthStateChanged:signed_in:" + user.getUid());
                 } else {
                     // User is signed out
-                    Log.d(LOG_TAG, "onAuthStateChanged:signed_out");
+                    Log.d(String.valueOf(getText(R.string.LOG_TAG_AUTH)), "onAuthStateChanged:signed_out");
                 }
-                // ...
             }
         };
     }
@@ -72,15 +68,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
-    public boolean validatePassword(String password) {
-        return password.length() > 6;
-    }
-
-    public boolean validateEmail(String email) {
-        matcher = pattern.matcher(email);
-        return matcher.matches();
-    }
-
     @Override
     public void onClick(View v) {
         hideKeyboard();
@@ -88,15 +75,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         String username = usernameEditText.getText().toString();
         String password = passwordEditText.getText().toString();
 
-        if (!validateEmail(username)) {
+        ValidationOfAuth validate = new ValidationOfAuth();
+
+        if (!validate.validateEmail(username)) {
             Toast.makeText(getApplicationContext(), "Not a valid email address!", Toast.LENGTH_SHORT).show();
-            //usernameEditText.setError("Not a valid email address!");
-        } else if (!validatePassword(password)) {
+        } else if (!validate.validatePassword(password)) {
             Toast.makeText(getApplicationContext(), "Not a valid password!", Toast.LENGTH_SHORT).show();
-            //usernameEditText.setError("Not a valid password!");
         } else {
-            //usernameEditText.setErrorEnabled(false);
-            //usernameEditText.setErrorEnabled(false);
             singing(username, password);
         }
     }
---
+++ app/src/main/java/com/fallenangel/linmea/authentication/ValidationOfAuth.java
@@ -0,0 +1,24 @@
+package com.fallenangel.linmea.authentication;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by NineB on 8/30/2017.
+ */
+
+public class ValidationOfAuth {
+
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9#_~!$&'()*+,;=:.\"(),:;<>@\\[\\]\\\\]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*$";
+    private Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+    private Matcher matcher;
+
+    public boolean validatePassword(String password) {
+        return password.length() > 6;
+    }
+
+    public boolean validateEmail(String email) {
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
